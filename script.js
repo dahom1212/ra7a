7cea38a3e40fe61a7274e77308b2f6acfdbc6ad8
Ra7a <DAHOMKRTON@gmail.com>
2026-01-21 09:42:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,26 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+
+/* ======= Modal ======= */
+const orderBtn = document.getElementById("openOrderModal");
+const orderBtn2 = document.getElementById("openOrderModal2");
+const modal = document.getElementById("orderModal");
+const closeModal = document.getElementById("closeModal");
+
+orderBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  modal.classList.add("show");
+});
+orderBtn2.addEventListener("click", (e) => {
+  e.preventDefault();
+  modal.classList.add("show");
+});
+
+closeModal.addEventListener("click", () => {
+  modal.classList.remove("show");
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) modal.classList.remove("show");
+});
